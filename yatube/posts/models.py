82ecae7c5c8c19b477f3faf7a0bc3225bf2d211b
LeAries <learies@ya.ru>
2022-03-04 17:16:42 +0300

    заменил список на картеж в Meta для модели Follow

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -106,7 +106,7 @@ class Follow(models.Model):
 
     class Meta:
         constraints = [models.UniqueConstraint(
-            fields=['user', 'author'],
+            fields=('user', 'author'),
             name='unique_follow',
         )]
 
