c30aa7d1785589f13874f47fad8ea0a2fe5359ed
LeAries <learies@ya.ru>
2022-03-04 21:44:19 +0300

    добавил verbose_name в модели Follow

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -107,6 +107,8 @@ class Follow(models.Model):
     )
 
     class Meta:
+        verbose_name = 'Подписки'
+        verbose_name_plural = 'Подписки'
         constraints = [
             models.UniqueConstraint(
                 fields=['user', 'author'],
